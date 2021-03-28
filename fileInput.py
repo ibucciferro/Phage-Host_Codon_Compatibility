db733c0fbe8eec6a705524cf80d02a82d6dfd97a
ecrum19 <49316983+ecrum19@users.noreply.github.com>
2021-03-28 10:15:07 -0500

    Add files via upload

---
+++ fileInput.py
@@ -0,0 +1,23 @@
+import os
+
+
+def parse(file_path):
+    # Opens fasta CDS files, parses them and returns a dictionary containing the gene name and sequence
+    f = open(file_path, 'r')
+    fs = f.read().strip().split('>')
+    final_genes = {}
+    for i in fs[1:]:
+        si = i.split(' ')
+        for comp in si:
+            if 'gbkey' in comp:
+                final_genes[si[0][4:]] = comp[12:].replace('\n', '')
+    return final_genes
+
+# Test
+i = 'Phage.txt'
+j = 'Bacteria.txt'
+parse(i)
+parse(j)
+
+
+
