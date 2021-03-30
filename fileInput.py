28de35b491cf02d7de767453bc41ec34ca5136c2
ecrum19 <49316983+ecrum19@users.noreply.github.com>
2021-03-30 15:51:41 -0500

    Add files via upload
    added a getFasta method

--- fileInput.py
+++ fileInput.py
@@ -1,8 +1,29 @@
 import os
+from Bio import Entrez, SeqIO
+Entrez.email = "ecrum@luc.edu"
+
+curr_path = os.getcwd()
 
 
-def parse(file_path):
+def getFasta(accession):
+    # Creates a fasta CDS file from a provided genbank accession
+
+    handle = Entrez.efetch(db="nucleotide", id=accession, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
+
+    inSeq = curr_path + '/' + accession + '.fasta'
+    getSeq = open(inSeq, 'w')
+    for rec in record.features:     # iterate through list of features
+        if rec.type == "CDS":
+            getSeq.write(">" + str(record.id) + "\n")               # gene IDs
+            getSeq.write(str(rec.extract(record.seq)) + "\n")       # gene sequence
+    getSeq.close()
+
+
+def parseFasta(file_path):
     # Opens fasta CDS files, parses them and returns a dictionary containing the gene name and sequence
+
     f = open(file_path, 'r')
     fs = f.read().strip().split('>')
     final_genes = {}
@@ -13,11 +34,6 @@ def parse(file_path):
                 final_genes[si[0][4:]] = comp[12:].replace('\n', '')
     return final_genes
 
-# Test
-i = 'Phage.txt'
-j = 'Bacteria.txt'
-parse(i)
-parse(j)
 
 #create a definition to get the Key value for the dictionary
 def GetKey(val):
@@ -27,9 +43,10 @@ def GetKey(val):
       return "key doesn't exist"
 
 #create a definition to take the parsed file from the Phage,
-#take each gene and 
+#take each gene and
 def phageDict(phageGeneList):
     for gene in phageGeneList:
-        
+
+
 
 
