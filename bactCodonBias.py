cdd043108161107f063edd087a8386a5e74daa71
ecrum19 <49316983+ecrum19@users.noreply.github.com>
2021-04-04 09:19:13 -0500

    Add files via upload

---
+++ bactCodonBias.py
@@ -0,0 +1,42 @@
+from Bio import SeqIO
+import pandas
+import os
+import subprocess
+
+cwd = '/Users/eliascrum/Programs/PythonProjects/COMP483/PHCC'
+## Uses DIAMOND on the database called testDB that contains a database assembled from the identical protein groups NCBI database of the
+## 40 highly expressed genes in bacteria. Outputs a file called matches with the sequence title, bitscore, and query sequence id. K is specified to avoid redundancy and get
+## the top hit for each query. Diamond has a binary in the parent working directory in this application. Diamond could be installed to avoid this change in directory.
+
+def _get_hegs(file):
+    os.chdir(cwd)
+    subprocess.call(
+        ["./diamond", "blastx", "-d", "testDB", "-q", cwd + "/" + file, "-o", cwd + "/matches", "-f",
+         "6", "stitle", "bitscore", "qseqid", "-k", "1"])
+
+
+_get_hegs('CP014272.fasta')
+
+
+def _get_hegs_to_forty(bact_genes):
+    df = pandas.read_table("matches", names=["Subject", "Bit", "SeqID"], skipinitialspace=True)
+    df = df.replace('\[.*\]', '', regex=True)
+    df["Subject"] = df["Subject"].str.strip()
+    df["Subject"] = df["Subject"].apply(lambda x: ' '.join(x.split(' ')[1:]))
+    df["Subject"] = df["Subject"].str.lower()
+    df = df.replace("elongation factor ef-2", "elongation factor g")
+    df2 = df.sort_values(["Subject", "Bit"], ascending=[True, False])
+    df2 = df2.loc[df2.groupby('Subject')["Bit"].idxmax()].reset_index(drop=True)
+    items = df2.SeqID.unique()
+    newSeqs = []
+    for seq_record in SeqIO.parse(self.file, "fasta"):
+        if (seq_record.id in items):
+            newSeqs.append(seq_record)
+    if len(newSeqs) < 38:
+        print("WARNING there are less than 38 sequences.")
+    with open("HEGS.fasta", "w") as handle:
+        SeqIO.write(newSeqs, handle, "fasta")
+    return len(newSeqs)
+
+
+
