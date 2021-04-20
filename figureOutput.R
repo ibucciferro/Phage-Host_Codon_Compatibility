c7d95bceafa110fd26d805e183a2718cfc92a0f1
Isabella Bucciferro <50893921+ibucciferro@users.noreply.github.com>
2021-04-20 16:16:47 -0500

    Create figureOutput.R

---
+++ figureOutput.R
@@ -0,0 +1,12 @@
+# read in the file (phage host codon correlation) to a data table (and remove the first four lines)
+datatable <- read.table(file = 'phage_host_codon_correlation.txt', header = FALSE, sep = '\t', skip = 4)
+
+#rename the columns in the data table
+colnames(datatable) <- c("Gene", "Corr")
+
+#use ggplot to create the box plot
+library(ggplot2)
+dev <- ggplot(datatable, aes(y = Corr, x = "")) + geom_boxplot() + coord_flip() + ylab("Codon Correlation") + xlab('')
+
+#save the file as a PNG
+dev.print(file="corrPlot.png", device=png, width=600)
